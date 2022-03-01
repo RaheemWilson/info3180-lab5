683f0be1b0c34fbb18f015dd7a794210e416dcf8
RaheemWilson <wilsonzackery16@gmail.com>
2022-03-01 08:20:20 -0500

    Added logout route

--- app/views.py
+++ app/views.py
@@ -61,10 +61,16 @@ def login():
 @app.route('/secure-page')
 @login_required
 def secure_page():
-    return render_template("secure-page.html")
+    return render_template("secure_page.html")
 
 
-
+# Logout route
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.', 'danger')
+    return redirect(url_for('home'))
 # user_loader callback. This callback is used to reload the user object from
 # the user ID stored in the session
 @login_manager.user_loader
